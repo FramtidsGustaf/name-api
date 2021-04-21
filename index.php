7bc98ec28d491cb88172d871c5ae1dfec1b4322a
GustafJ <gustaf89@hotmail.se>
2021-04-21 14:06:41 +0200

    add cool things

--- index.php
+++ index.php
@@ -6,11 +6,28 @@ header("Access-Control-Allow-Methods: GET");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 header("Referrer-Policy: no-referrer");
 
-$firstNames = ["Robin", "Mahmud", "Kevin", "Sebastian", "Gustaf", "Emma", "Folke", "Marita", "Linnea", "Karin"];
+$firstnamesFemale = ["Åsa", "Lotta", "Greta", "Amanda", "Marta", "Maja", "Nina", "Erika", "Deepthi", "Inna"];
+$firstnamesMale = ["Robin", "Mahmud", "Kevin", "Sebastian", "Gustaf", "Björn", "Erik", "Dragan", "Özgur", "Dino"];
 $lastNames = ["Johnsson", "Hedlund", "Al Hakim", "Gedda", "Basele", "Carlson", "Bernadotte", "Wonder", "Richie", "Olsson"];
 
 for ($i = 0; $i < 10; $i++) {
-  $names[] = array("firstname" => $firstNames[rand(0, 9)], "lastname" => $lastNames[rand(0, 9)]);
+  $random = rand(0, 1);
+  $firstNames = $random ? $firstnamesFemale : $firstnamesMale;
+  $firstName = $firstNames[rand(0, 9)];
+  $lastName = $lastNames[rand(0, 9)];
+  $search = 'åäöÅÄÖ';
+  $replace = 'aaoAAO';
+
+  $email = strtolower(substr(strtr($firstName, $search, $replace), 0, 2) . substr(strtr($lastName, $search, $replace), 0, 3)) . '@example.com';
+  // $email = mb_strtolower(mb_substr($firstName, 0, 2, 'UTF-8') . mb_substr($lastName, 0, 3, 'UTF-8'), 'UTF-8') . '@example.com';
+
+  $names[] = array(
+    "firstname" => $firstName,
+    "lastname" => $lastName,
+    "gender" => $random ? "female" : "male",
+    "age" => rand(1, 100),
+    "email" => $email
+  );
 }
 
 echo json_encode($names, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
