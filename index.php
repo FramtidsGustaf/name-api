0d801551e2bba1a23495e1d99cc4079ea9870a4b
GustafJ <gustaf89@hotmail.se>
2021-04-21 14:32:07 +0200

    change variable names to suit Maja

--- index.php
+++ index.php
@@ -6,13 +6,13 @@ header("Access-Control-Allow-Methods: GET");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 header("Referrer-Policy: no-referrer");
 
-$firstnamesFemale = ["Åsa", "Lotta", "Greta", "Amanda", "Marta", "Maja", "Nina", "Erika", "Deepthi", "Inna"];
-$firstnamesMale = ["Robin", "Mahmud", "Kevin", "Sebastian", "Gustaf", "Björn", "Erik", "Dragan", "Özgur", "Dino"];
+$female = ["Åsa", "Lotta", "Greta", "Amanda", "Marta", "Maja", "Nina", "Erika", "Deepthi", "Inna"];
+$male = ["Robin", "Mahmud", "Kevin", "Sebastian", "Gustaf", "Björn", "Erik", "Dragan", "Özgur", "Dino"];
 $lastNames = ["Johnsson", "Hedlund", "Al Hakim", "Gedda", "Basele", "Carlson", "Bernadotte", "Wonder", "Richie", "Olsson"];
 
 for ($i = 0; $i < 10; $i++) {
   $random = rand(0, 1);
-  $firstNames = $random ? $firstnamesFemale : $firstnamesMale;
+  $firstNames = $random ? $female : $male;
   $firstName = $firstNames[rand(0, 9)];
   $lastName = $lastNames[rand(0, 9)];
   $search = 'åäöÅÄÖ';
